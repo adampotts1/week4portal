3a61ec0a20b85ad529ef664d7f88bb1e4d3dd5ce
adampotts1 <121840447+adampotts1@users.noreply.github.com>
2023-02-01 20:00:26 -0700

    Add files via upload

---
+++ script/darkMode.js
@@ -0,0 +1,14 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#5c95b9";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
